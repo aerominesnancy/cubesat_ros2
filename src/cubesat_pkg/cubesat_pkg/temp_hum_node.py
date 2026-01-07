616b4c97cea67792e3dda61fd22c870ab4f01f91
Paul-R57 <paul.rr@numericable.fr>
2026-01-07 11:52:29 +0100

    test adafruit_dht lib without board

--- src/cubesat_pkg/cubesat_pkg/temp_hum_node.py
+++ src/cubesat_pkg/cubesat_pkg/temp_hum_node.py
@@ -26,7 +26,7 @@ class TemperatureHumidityNode(Node):
             raise ValueError("Parameter 'callback_delay_second' must be set to a valid delay in seconds.")
 
         # create sensor instance
-        self.sensor = adafruit_dht.DHT11(board.D4)  # pin 4 BCM numbering
+        self.sensor = adafruit_dht.DHT11(4)  # pin 4 BCM numbering
 
         # publishers for temperature and humidity
         self.temp_pub = self.create_publisher(Float32, f"/temp_hum_sensor_{self.sensor_number}/temperature", 10)
--- src/cubesat_pkg/launch/launch_cubesat.py
+++ src/cubesat_pkg/launch/launch_cubesat.py
@@ -7,7 +7,7 @@ from launch_ros.actions import Node
 import os
 
 # change log format
-os.environ['RCUTILS_CONSOLE_OUTPUT_FORMAT'] = '{time}\t[{name}] {severity} \t{message}'
+os.environ['RCUTILS_CONSOLE_OUTPUT_FORMAT'] = '{time}\t{severity} [{name}] \t{message}'
 
 # This function is always needed
 def generate_launch_description():

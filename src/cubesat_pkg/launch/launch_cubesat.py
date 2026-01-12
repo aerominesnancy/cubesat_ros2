d7e08cf91556c7e23358ee4720f67d12c930254a
Paul-R57 <paul.rr@numericable.fr>
2026-01-12 12:35:11 +0100

    test launch avec camera

--- src/cubesat_pkg/launch/launch_cubesat.py
+++ src/cubesat_pkg/launch/launch_cubesat.py
@@ -32,6 +32,12 @@ def generate_launch_description():
     parameters=[{'sensor_number': 1, 'gpio_pin': 4, 'callback_delay_second': 2.0}]
   )
 
+  camera_node = Node(
+    package="cubesat_pkg",
+    executable="camera_node",
+    parameters=[{'callback_delay_second': 5.0}]
+  )
+
   # Add the nodes and the process to the LaunchDescription list
-  ld = [imu_node,   motor_node, temp_hum_node_1]
+  ld = [imu_node, temp_hum_node_1, camera_node]
   return LaunchDescription(ld)

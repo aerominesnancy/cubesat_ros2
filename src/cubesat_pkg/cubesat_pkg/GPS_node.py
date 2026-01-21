d674c8e9127855499de5298c37efa7b97235c64b
Paul-R57 <paul.rr@numericable.fr>
2026-01-21 09:56:59 +0100

    bug fix

--- src/cubesat_pkg/cubesat_pkg/GPS_node.py
+++ src/cubesat_pkg/cubesat_pkg/GPS_node.py
@@ -29,9 +29,10 @@ class GPS(Node):
             if not line:
                 self.get_logger().warn(f"No data received from GPS module.")
                 return
-            self.get_logger().info(f'Received GPS data : {line}')
             
             nmea = self.parse_nmea_sentence(line)
+            if not nmea:
+                self.get_logger().warn(f"Invalid NMEA sentence.")
             self.get_logger().info(f'GPS data decoded : {nmea}')
 
 

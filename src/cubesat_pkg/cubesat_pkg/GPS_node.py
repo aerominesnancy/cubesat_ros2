3a7df9d08c5b48187beb9794f115bba01e5ff8f6
Paul-R57 <paul.rr@numericable.fr>
2026-01-22 11:22:53 +0100

    te

--- src/cubesat_pkg/cubesat_pkg/GPS_node.py
+++ src/cubesat_pkg/cubesat_pkg/GPS_node.py
@@ -164,7 +164,7 @@ class GPS(Node):
                     self.get_logger().warn(f"Invalid NMEA sentence. {data}")
                 else:
                     self.get_logger().info(f'GPS data decoded !')
-                    self.print_gps_logs(nmea)
+                    #self.print_gps_logs(nmea)
 
         except Exception as e:
             self.get_logger().error(f"Error parsing NMEA sentence: {e}")

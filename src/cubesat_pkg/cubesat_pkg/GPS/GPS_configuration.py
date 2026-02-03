9dad84f8b01c31d6d8785bf70f4bf81f6607b4e8
Paul-R57 <paul.rr@numericable.fr>
2026-02-03 15:02:48 +0100

    test gps config

--- src/cubesat_pkg/cubesat_pkg/GPS/GPS_configuration.py
+++ src/cubesat_pkg/cubesat_pkg/GPS/GPS_configuration.py
@@ -1,11 +1,14 @@
 import serial
 import time
-
+"""
+Le plus simple c'est d'avoir un adaptateur USB/UART
+et d'utiliser le logiciel (windows) u-center de u-blox
+"""
 ser = serial.Serial('/dev/ttyAMA1', baudrate=9600, timeout=1)
 
 # Envoyer une commande UBX pour lire la configuration
 command = b'\xB5\x62\x06\x00\x00\x00\x1A\x2E'
-ser.write(command)
+#ser.write(command)
 
 # Lire la réponse
 time.sleep(2)
